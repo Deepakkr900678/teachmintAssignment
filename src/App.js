086b6658cbccde8790687d26b7d2df740a5f3126
DEEPAK KUMAR <112412315+Deepakkr900678@users.noreply.github.com>
2024-01-04 17:35:51 +0530

    teachmintAssignment Added

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Directory from './components/Directory';
+import UserDetails from './components/UserDetails';
+
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Directory} />
+          <Route
+            exact
+            path="/user/:id"
+            component={UserDetails}
+          />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
